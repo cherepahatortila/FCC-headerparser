4f3f530a61e55e899b34318e5b275115c7111410
cherepahatortila <59018238+cherepahatortila@users.noreply.github.com>
2021-11-14 00:10:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  
+  res.send({ipaddress:req.ip, language:req.header("accept-language"), software:req.header('user-agent')
+   })
 });
 
 
